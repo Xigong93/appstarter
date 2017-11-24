68fb07ae6d1388fecefb1125d9af35f602d62f8c
pokercc <pokercc>
2017-11-25 02:35:49 +0800

    准备进行android manifest 的单元测试

---
+++ appstarter/src/test/java/com/pokercc/appstarter/ManifestAppEntryFinderTest.java
@@ -0,0 +1,15 @@
+package com.pokercc.appstarter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by cisco on 2017/11/25.
+ */
+public class ManifestAppEntryFinderTest {
+    @Test
+    public void getAppInjectors() throws Exception {
+    }
+
+}
